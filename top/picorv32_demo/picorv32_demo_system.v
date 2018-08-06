436eeb46cdb931afb4c7ee5fe0819892e4259020
Lucas Russo <lerwys@gmail.com>
2018-08-06 12:45:53 -0700

    picorv32_demo_system: add CTI/BTE signals to modules that use them

--- top/picorv32_demo/picorv32_demo_system.v
+++ top/picorv32_demo/picorv32_demo_system.v
@@ -69,6 +69,8 @@ wb_ram #(
     .wb_dat_i           (wb_s2m_ram_dat),
     .wb_sel_i           (wb_m2s_ram_sel),
     .wb_we_i            (wb_s2m_ram_we),
+    .wb_bte_i           (wb_m2s_ram_bte),
+    .wb_cti_i           (wb_m2s_ram_cti),
     .wb_adr_i           (wb_m2s_ram_adr),
     .wb_stb_i           (wb_m2s_ram_stb),
     .wb_dat_o           (wb_s2m_ram_dat),
